cbc62cbb2ced02253c81fa1f2f6ead4af0ee5e63
Doriana <a_dorianaangelica.dacostamagello@iespablopicasso.es>
2023-02-03 13:27:00 +0100

    añadir metodo devolver marca

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -4,13 +4,18 @@ public class Vehiculo {
   private String marca;
   private String modelo;
   private String matricula;
-  
-  
+
+
   public Vehiculo(String marca, String modelo, String matricula) {
     super();
     this.marca = marca;
     this.modelo = modelo;
     this.matricula = matricula;
+  }
+
+  public String mostrarMarca(String marca) {
+    return marca;
+
   }
 
 
@@ -19,6 +24,6 @@ public class Vehiculo {
     return "Vehiculo [marca=" + marca + ", modelo=" + modelo + ", matricula=" + matricula + "]";
   }
 
-  
+
 }
 
